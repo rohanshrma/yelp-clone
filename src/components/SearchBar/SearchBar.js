c6fd4b78302f432a7fa67f9aac97b69ae9f18c29
Rohan Sharma <rs.sharma2014@gmail.com>
2021-03-03 10:14:00 -0500

    merging first feature

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import './SearchBar.css';
+
+const SortByOptions {
+    'Best Match': 'best_match',
+    'Highest Rated': 'rating',
+    'Most Reviewed': 'review_count'
+}
+
+class SearchBar extends React.Component{
+    renderSortByOptions() {
+        return Object.keys(SortByOptions).map(SortByOptions => {
+            let SortByOptionValue = SortByOptions[sortByOption];
+            return <li key={SortByOptionValue}> {sortByOption} </li>;
+        });
+    }
+    render() {
+        return(
+            <div className="SearchBar">
+                <div className="SearchBar-sort-options">
+                    <ul>
+                    {SearchBar.renderSortByOptions()}; //step 42
+                    </ul>
+                </div>
+                <div className="SearchBar-fields">
+                    <input placeholder="Search Businesses" />
+                    <input placeholder="Where?" />
+                </div>
+                <div className="SearchBar-submit">
+                    <a>Let's Go</a>
+            </div>
+            </div>
+        );
+    }
+};
